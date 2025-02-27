c12f54a428ebe7b334285a771bb6e98c90e23138
Christian Hatch <christianhatch@gmail.com>
2025-02-26 21:39:04 -0500

    canonical swift

--- FriendsWeekly/Constants.swift
+++ FriendsWeekly/Constants.swift
@@ -7,11 +7,13 @@
 
 import Foundation
 
-let productionURL = "https://friendsweekly.com"
-let localURL = "http://localhost:3000"
-let stuURL = "https://stu.ngrok.io"
-
 enum Env {
     static let baseURL = URL(string: productionURL)! // Change this variable to update local vs prod
-	static let remotePathConfigURL = Env.baseURL.appending(path: "configurations/ios_v1.json")
+	static let remotePathConfigURL = baseURL.appending(path: "configurations/ios_v1.json")
+}
+
+extension Env {
+	private static let productionURL = "https://friendsweekly.com"
+	private static let localURL = "http://localhost:3000"
+	private static let stuURL = "https://stu.ngrok.io"
 }
