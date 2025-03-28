897650d07f1ebcc85344bc40a9fa98c044c53dce
Christian Hatch <christianhatch@gmail.com>
2025-03-28 15:37:28 -0400

    Auto generate TestFlight release notes on CI

---
+++ FriendsWeekly/ci_scripts/ci_post_xcodebuild.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  ci_post_xcodebuild.sh
+# Run after the xcodebuild phase of the CI process, before the TestFlight upload
+
+# Update the TestFlight release notes with the last 3 commit messages
+if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
+  TESTFLIGHT_DIR_PATH=../TestFlight
+  mkdir $TESTFLIGHT_DIR_PATH
+  git fetch --deepen 3 && git log -3 --pretty=format:"%s" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+fi
