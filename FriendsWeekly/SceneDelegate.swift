64ab51eca3bf53457e2df5250dab97acd1501528
Christian Hatch <christianhatch@gmail.com>
2025-03-28 15:26:06 -0400

    Universal Links!

--- FriendsWeekly/SceneDelegate.swift
+++ FriendsWeekly/SceneDelegate.swift
@@ -12,9 +12,36 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     private let navigator = Navigator()
 
+	/// Called on app launch
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         Style.configure()
         window?.rootViewController = navigator.rootViewController
-        navigator.route(Env.baseURL)
+
+		if let userActivity = connectionOptions.userActivities.first, let url = getUniversalLink(userActivity) {
+			routeTo(url)
+		} else {
+			routeTo(Env.baseURL)
+		}
     }
+
+	/// Called when the app is running
+	func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+		if let url = getUniversalLink(userActivity) {
+			routeTo(url)
+		}
+	}
+
+	/// Extract the universal link from the given user activity
+	private func getUniversalLink(_ userActivity: NSUserActivity) -> URL? {
+		guard userActivity.activityType == NSUserActivityTypeBrowsingWeb, let incomingURL = userActivity.webpageURL else {
+			return nil
+		}
+
+		return incomingURL
+	}
+
+	/// Route to the given URL
+	private func routeTo(_ url: URL) {
+		navigator.route(url)
+	}
 }
